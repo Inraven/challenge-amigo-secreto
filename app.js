0b9e476b6408e4e22eb6fc011b64e001540d7fe0
Angel Ruiz <inraveng@gmail.com>
2025-03-17 10:34:58 -0700

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//test
+let amigos = [];
+function agregarAmigo(){
+    const inputNombre = document.getElementById("amigo");
+    if(inputNombre.value.trim() === ""){
+        alert("Ingresa un nombre valido");
+        return;
+    }
+    amigos.push(inputNombre.value);
+    inputNombre.value = "";
+    
+    //console.log(amigos);
+}
